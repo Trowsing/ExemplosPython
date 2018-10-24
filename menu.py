1627f82c3a9825d611b2df24f47aca4c71002a4f
Jhonathan Ballesteros <43155237+Trowsing@users.noreply.github.com>
2018-10-23 20:38:18 -0400

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,58 @@
+import string
+senhaN = 100
+senhaP = 0
+turnos = []
+
+def restart():
+    senhaP = 0
+    senhaN = 0
+    print("Contagem reiniciada: N = ", senhaN,"P = ", senhaP)
+
+def incorreto():
+    print("Entrada invalida")
+    menu()
+
+def menu():
+    global senhaP
+    global senhaN
+    print("Bem vindo, escolha uma opcao \n 1.- Admin \n 2.- Usuario")
+    opcao = input()
+    if opcao == 1:
+        print("Admin, escolha uma opcao: \n 1.- Reiniciar contagem \n 2.- Proximo")
+        opAdmin = input()
+        if opAdmin == 1:
+            restart()
+            menu()
+        if opAdmin == 2:
+            if senhaP > 0:
+                print("Chamando preferencial: ", turnos[0])
+                turnos.pop(0)
+                menu()
+            else:
+                print("Chamada normal: ", turnos[0])
+                turnos.pop(0)
+                menu()
+        else:
+            incorreto()
+    if opcao == 2:
+        print("Usuario, escolha uma opcao: \n 1.- Senha normal \n 2.- Senha preferencial")
+        opUsuario = input()
+        if opUsuario == 1:
+            senhaN += 1
+            turnos.append(senhaN)
+            print("N", senhaN)
+            print(turnos)
+            menu()
+        if opUsuario == 2:
+            if senhaP == 99:
+                senhaP = 0
+            senhaP += 1
+            turnos.append(senhaP)
+            turnos.sort()
+            print("P", senhaP)
+            print(turnos)
+            menu()
+    else:
+        incorreto()
+
+menu()
