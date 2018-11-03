f1cb178e92266150525712a41e43e5a9a6ea9702
Jhonathan <trowsing@gmail.com>
2018-11-03 14:55:01 -0400

    Fibonacci commir

---
+++ prime-sum.py
@@ -0,0 +1,12 @@
+# print all the even Fibonacci numbers up to n
+# and add each even number found to the sum.
+
+a, b, suma = 1, 2, 0
+n = input("Max limit to sum: ")
+
+while (a<=n):
+    if a % 2 == 0:
+        print (a)
+        suma += a
+    a, b = b, a+b
+print("The total is: ", suma)
