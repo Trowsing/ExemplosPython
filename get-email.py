80aa968deba45607d8fe7741eb3addd9578f2157
Jonathan <trowsing@gmail.com>
2018-11-25 20:33:48 -0400

    Code addition

---
+++ get-email.py
@@ -0,0 +1,10 @@
+# Getting email from string
+ 
+import re
+
+pattern = r"([\w\.-]+)@([\w\.-]+)(\.[\w.]+)"
+str = "Please contact anything@gmail.com for assistance"
+
+match = re.search(pattern, str)
+if match:
+    print(match.group())
