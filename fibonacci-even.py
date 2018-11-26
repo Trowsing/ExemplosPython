50d87d730fc610d06cdd9ca289e81312bca8ad68
Jonathan <trowsing@gmail.com>
2018-11-25 20:44:25 -0400

    Code addition #2

---
+++ fibonacci-even.py
@@ -0,0 +1,11 @@
+# Fibonacci even sum to given range
+ 
+ran = int(input("Sum even in range: "))
+a, b, suma = 1, 2, 0
+
+while (a<=ran):
+    if a % 2 == 0:
+        print (a)
+        suma += a
+    a, b = b, a+b
+print(suma)
---
+++ reverse-string.py
@@ -0,0 +1,5 @@
+# This converts the string backwards
+
+string = input(print("Introduce texto"))
+reverse = string[::-1]
+print("La inversion es: ",reverse)
